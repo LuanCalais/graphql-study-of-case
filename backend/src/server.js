f8aee1a9c3a81f3f14ba8cf350d2b7eea85d8bab
LuanCalais <luancalais@gmail.com>
2025-06-14 10:58:06 -0300

    feat: init create vite component to integrate

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,8 +17,8 @@ async function startServer() {
   await server.start();
   server.applyMiddleware({ app });
 
-  app.listen({ port: 4000 }, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  app.listen({ port: 4001 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4001${server.graphqlPath}`)
   );
 }
 
